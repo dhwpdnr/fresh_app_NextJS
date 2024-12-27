64f8afdb80b61c982ba248aafcf26dccc0d29f41
dhwpdnr <ink1237@gmail.com>
2024-12-27 20:16:34 +0900

    chore: props

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,23 +1,32 @@
 import { age, name } from "./data.js";
 import Hi from "./hi.js";
 export default function Cart() {
+  let carts = ["Tomato", "Pasta"];
   return (
     <div>
+      <Banner card="롯데카드" />
+      <Banner card="현대카드" />
       <h1 className="title">장바구니</h1>
       <Hi />
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      {carts.map((a, i) => {
+        return <CartItem itemName={a} />;
+      })}
     </div>
   );
 }
-
-function CartItem() {
+function Banner(props) {
+  return <p>{props.card} 무이자 이벤트 중</p>;
+}
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.itemName}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
   );
+}
+
+function Btn(props) {
+  return <button style={{ background: props.color }}>버튼임</button>;
 }
