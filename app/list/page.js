78cc8b0d1d7b3efa4f99b5c6e073c6ba1fc641a2
dhwpdnr <ink1237@gmail.com>
2024-12-28 15:23:18 +0900

    chore: use state

--- app/list/page.js
+++ app/list/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function List() {
   let item = ["Tomato", "Pasta", "Coconut"];
-  let arr = [2, 3, 4];
-  arr.map((a, i) => {});
+  let [count, setCount] = useState(0);
   return (
     <div>
       <h4 className="title">상품 목록</h4>
@@ -12,6 +13,21 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{a} $40</h4>
+            <span> {count} </span>
+            <button
+              onClick={() => {
+                setCount(count - 1);
+              }}
+            >
+              -
+            </button>
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
